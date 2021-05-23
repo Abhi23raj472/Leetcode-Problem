7f1a77bef4b3ffb33c1ba88fc648fbbe0324319b
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2021-05-23 12:41:17 +0530

    Create 69. Sqrt(x).cpp

---
+++ 69. Sqrt(x).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long int mySqrt(long long int x) {
+        if(x==0||x==1){
+            return x;
+        }
+        long long int i=1,result=1;
+        while(result<=x){
+            i++;
+            result=i*i;
+        }
+        return i-1;
+    }
+};
