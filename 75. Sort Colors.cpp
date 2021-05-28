3ab4c6f934947d37f25a7e07b453fb90ee384d9f
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2021-05-28 10:11:46 +0530

    Create 75. Sort Colors.cpp

---
+++ 75. Sort Colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i,j;
+        bool swapped=false;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=0;j<nums.size()-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+};
