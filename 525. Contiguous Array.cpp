c104ac81ed9a2412ff90a2968661e47c9dda1436
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2021-06-08 23:56:02 +0530

    Create 525. Contiguous Array.cpp

---
+++ 525. Contiguous Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> subSum;
+        int sum = 0;
+        int ans = 0;
+        subSum[0] = 0;
+        for(int i=1;i<=nums.size();i++){
+            sum += nums[i-1] == 0 ? -1 : 1;
+            if(subSum.find(sum) != subSum.end()){
+                ans = max(ans, i - subSum[sum]);
+            } else {
+                subSum[sum] = i;
+            }
+        }
+        return ans;
+    }
+};
