b254753c523924ee0c64a670ffff7ecdfcbcc7da
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2021-06-15 15:35:03 +0530

    Create 50. Pow(x, n).cpp

---
+++ 50. Pow(x, n).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.0;
+        long a=n;
+        if(a<0){
+            a=-1*a;
+        }
+        while(a>0){
+            if(a%2==1){
+                ans=ans*x;
+                a=a-1;
+            }
+            else{
+                x=x*x;
+                a=a/2;
+            }
+        }
+        if(n<0){
+            ans=(double)(1.0)/(double)(ans);
+            
+        }
+        return ans;
+    }
+};
